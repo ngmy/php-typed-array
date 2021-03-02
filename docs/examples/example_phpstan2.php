2cd0ad5b0adc0107523e7eddf2c7641ed3f2fd40
Yuta Nagamiya <y.nagamiya@gmail.com>
2021-03-02 18:15:21 +0900

    Update

--- docs/examples/example_phpstan2.php
+++ docs/examples/example_phpstan2.php
@@ -4,34 +4,34 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new());                      // TypedArray<mixed, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withBoolKey());       // TypedArray<bool, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withFloatKey());      // TypedArray<float, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withIntKey());        // TypedArray<int, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withMixedKey());      // TypedArray<mixed, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withObjectKey());     // TypedArray<object, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withResourceKey());   // TypedArray<resource, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withStringKey());     // TypedArray<string, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new());                      // Ngmy\TypedArray\TypedArray<mixed, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withBoolKey());       // Ngmy\TypedArray\TypedArray<bool, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withFloatKey());      // Ngmy\TypedArray\TypedArray<float, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withIntKey());        // Ngmy\TypedArray\TypedArray<int, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withMixedKey());      // Ngmy\TypedArray\TypedArray<mixed, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withObjectKey());     // Ngmy\TypedArray\TypedArray<object, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withResourceKey());   // Ngmy\TypedArray\TypedArray<resource, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withStringKey());     // Ngmy\TypedArray\TypedArray<string, mixed>
 PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()
-    ->withClassKey(Ngmy\TypedArray\Tests\Data\Class1::class));            // TypedArray<Ngmy\TypedArray\Tests\Data\Class1, mixed>
+    ->withClassKey(Ngmy\TypedArray\Tests\Data\Class1::class));            // Ngmy\TypedArray\TypedArray<Ngmy\TypedArray\Tests\Data\Class1, mixed>
 PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()
-    ->withInterfaceKey(Ngmy\TypedArray\Tests\Data\Interface1::class));    // TypedArray<Ngmy\TypedArray\Tests\Data\Interface1, mixed>
+    ->withInterfaceKey(Ngmy\TypedArray\Tests\Data\Interface1::class));    // Ngmy\TypedArray\TypedArray<Ngmy\TypedArray\Tests\Data\Interface1, mixed>
 PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()
-    ->withTraitKey(\Ngmy\TypedArray\Tests\Data\Trait1::class));           // TypedArray<object, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withArrayValue());    // TypedArray<mixed, array<int|string, mixed>>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withBoolValue());     // TypedArray<mixed, bool>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withFloatValue());    // TypedArray<mixed, float>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withIntValue());      // TypedArray<mixed, int>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withMixedValue());    // TypedArray<mixed, mixed>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withObjectValue());   // TypedArray<object, object>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withResourceValue()); // TypedArray<mixed, resource>
-PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withStringValue());   // TypedArray<mixed, string>
+    ->withTraitKey(\Ngmy\TypedArray\Tests\Data\Trait1::class));           // Ngmy\TypedArray\TypedArray<object, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withArrayValue());    // Ngmy\TypedArray\TypedArray<mixed, array<int|string, mixed>>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withBoolValue());     // Ngmy\TypedArray\TypedArray<mixed, bool>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withFloatValue());    // Ngmy\TypedArray\TypedArray<mixed, float>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withIntValue());      // Ngmy\TypedArray\TypedArray<mixed, int>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withMixedValue());    // Ngmy\TypedArray\TypedArray<mixed, mixed>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withObjectValue());   // Ngmy\TypedArray\TypedArray<object, object>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withResourceValue()); // Ngmy\TypedArray\TypedArray<mixed, resource>
+PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()->withStringValue());   // Ngmy\TypedArray\TypedArray<mixed, string>
 PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()
-    ->withClassValue(Ngmy\TypedArray\Tests\Data\Class1::class));          // TypedArray<mixed, Ngmy\TypedArray\Tests\Data\Class1>
+    ->withClassValue(Ngmy\TypedArray\Tests\Data\Class1::class));          // Ngmy\TypedArray\TypedArray<mixed, Ngmy\TypedArray\Tests\Data\Class1>
 PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()
-    ->withInterfaceValue(Ngmy\TypedArray\Tests\Data\Interface1::class));  // TypedArray<mixed, Ngmy\TypedArray\Tests\Data\Interface1>
+    ->withInterfaceValue(Ngmy\TypedArray\Tests\Data\Interface1::class));  // Ngmy\TypedArray\TypedArray<mixed, Ngmy\TypedArray\Tests\Data\Interface1>
 PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()
-    ->withTraitValue(\Ngmy\TypedArray\Tests\Data\Trait1::class));         // TypedArray<mixed, object>
+    ->withTraitValue(\Ngmy\TypedArray\Tests\Data\Trait1::class));         // Ngmy\TypedArray\TypedArray<mixed, object>
 PHPStan\dumpType(Ngmy\TypedArray\TypedArray::new()
     ->withBoolKey()
-    ->withArrayValue());                                                  // TypedArray<bool, array<int|string, mixed>>
+    ->withArrayValue());                                                  // Ngmy\TypedArray\TypedArray<bool, array<int|string, mixed>>
