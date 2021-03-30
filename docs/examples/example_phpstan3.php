446779a24d12245301f97fa986e152fbd3bc5f33
Yuta Nagamiya <y.nagamiya@gmail.com>
2021-03-30 18:41:52 +0900

    Add examples for getIterator and toArray methods

---
+++ docs/examples/example_phpstan3.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$typedArray = Ngmy\TypedArray\TypedArray::new()
+    ->withIntKey()
+    ->withStringValue();
+foreach ($typedArray as $key => $value) {
+    PHPStan\dumpType($key);   // int|string
+    // NOTE: PHPStan does not yet support conditional types, so @phpstan-var is needed
+    /** @phpstan-var int $key */
+    PHPStan\dumpType($key);   // int
+    PHPStan\dumpType($value); // string
+}
+
+$array = $typedArray->toArray();
+PHPStan\dumpType($array); // array<int|string, string>
+// NOTE: PHPStan does not yet support conditional types, so @phpstan-var is needed
+/** @phpstan-var array<int, string> $array */
+PHPStan\dumpType($array); // array<int, string>
